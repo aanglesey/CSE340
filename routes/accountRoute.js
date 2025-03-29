17fcaf717dcae690bfd2f14e6bb1aecd4f1d6554
ash0345 <113393782+ash0345@users.noreply.github.com>
2025-03-28 14:12:04 -1000

    learning activity wk 4

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,18 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController.js")
-
-// const regValidate = require('../utilities/account-validation')
+const regValidate = require('../utilities/account-validation')
 
 // Login Account View Route
-router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Register Account View Route
-router.get("/register", utilities.handleErrors(accountController.buildRegister));
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+// process resgister account
+router.post(
+    "/register", 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const Util = require("./utilities")
 const session = require("express-session")
 const pool = require('./database/')
 const errorRoute = require("./routes/errorRoute")
-// const errorTest = require("./controllers/errorController")
 const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -39,6 +39,9 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine Templates
